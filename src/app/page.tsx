1cf30fc1aa15b2b5a287b5637b442bce47ffa5ed
sankha4567 <ssankha103@gmail.com>
2025-07-04 08:26:49 +0530

    part2.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@/components/ui/button";
 
 const Home = ()=>{
+  
   return (
     <div>
-   <Button variant="secondary">Click Me</Button>
-
+      Hello World!!
 
     </div>
   )
